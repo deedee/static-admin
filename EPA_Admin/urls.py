b3eae89ac84cd34eee2968523e2f12318fbfecb1
deedee <dsyefria@gmail.com>
2016-10-05 19:35:59 +0700

    add static

--- EPA_Admin/urls.py
+++ EPA_Admin/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -27,3 +30,5 @@ urlpatterns = patterns('',
     url(r'^$', 'epa.views.view_prediction_data')
 
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
